2cc0155d2793f924e55235433e5ada02c3063e41
Vamsi Sangam <vesangam@microsoft.com>
2025-06-24 14:52:09 -0700

    Modify step 4

--- dotNet/src/Program.cs
+++ dotNet/src/Program.cs
@@ -6,7 +6,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        var alerts = Mocks.MockedAlerts.GetMockedAlerts();
+        // Get the list of alerts (mocked ones for now)
+        Mocks.MockedAlerts.GetMockedAlerts();
+
         // In this exercise you should only be using *completions*. Do not use inline chat / chat
         // In this exercise you should only be using *inline chat*. Do not use completions / chat
         // In this exercise you should only be using Chat. Do not use completions / inline chat
