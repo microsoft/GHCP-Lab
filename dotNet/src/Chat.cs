34d03ba74e42f601534ce69ffe00a404966e4784
Vamsi Sangam <vesangam@microsoft.com>
2025-06-26 15:18:52 -0700

    Shortening and adding packages

--- dotNet/src/Chat.cs
+++ dotNet/src/Chat.cs
@@ -7,18 +7,9 @@ public class GHCPChat
 {
     public static void OnlyChat()
     {
-        // #1
-
-        // #2
-
-        // #3
         static double Divider(double numerator, double denominator)
         {
             return numerator / denominator;
         }
-        // #4
-
-        // #5
-
     }
 }
